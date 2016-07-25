bf0e4982bbb6f6f4c29f9c9e5cb8ebd75fddf060
seitzjoshua7 <seitzjoshua7@gmail.com>
2016-07-25 21:36:45 +0000

    fix landing and add background

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 var Comment = require("./models/comment");
-var seedDB = require("./seeds");
 
 
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://Joshua:Slamdunk14@ds029715.mlab.com:29715/yelpcamp");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://Joshua:Slamdunk14@ds029715.mlab.com:29715/yelpcamp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -103,7 +103,10 @@ app.post("/campgrounds/:id/comments", function(req, res){
    //create new comment
    //connect new comment to campground
    //redirect campground show page
-   
+});
+
+app.get("/other/about", function(req, res){
+    res.render("other/about");
 });
 
 
